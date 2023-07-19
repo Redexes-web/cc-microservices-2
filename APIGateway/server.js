fc67c45ff625a7b6f4df4ae8e5b4a8d39641db40
Enzo <e.valfort@epastelsoft.com>
2023-07-19 15:45:25 +0200

    .

--- APIGateway/server.js
+++ APIGateway/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const proxy = require('express-http-proxy');
-
 const adminMiddleware = require('./middlewares/adminMiddleware.js');
+
 app.use('/api/auth', proxy('http://authentication-service:8081'));
 app.use('/api/product', adminMiddleware, proxy('http://products-service:8082'));
 
