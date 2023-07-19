957d6972a2b91a7908b91c275346529f1e97e657
Enzo <e.valfort@epastelsoft.com>
2023-07-19 15:23:54 +0200

    ajout du middleware admin role sur les routes /products

--- Auth/App/middleware/auth.js
+++ Auth/App/middleware/auth.js
@@ -7,12 +7,12 @@ const auth = {
 		if (!token) {
 			return res.status(401).json({ error: 'Access denied' });
 		}
-		//remove "Bearer " from token
 		token = token.split(' ')[1];
-
 		try {
-			const decoded = jwt.verify(token, process.env.TOKEN_SECRET, { ignoreExpiration: true })
-            console.log(decoded)
+			const decoded = jwt.verify(token, process.env.TOKEN_SECRET, {
+				ignoreExpiration: true,
+			});
+			console.log(decoded);
 			req.user = decoded;
 			next();
 		} catch (error) {
